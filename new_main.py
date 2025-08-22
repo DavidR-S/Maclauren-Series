b726cc2fc4d892078f48ff95105a50a2f106d83b
David <driverssegovia@gmail.com>
2025-08-22 18:43:25 +0200

    Add sin and exp functions

--- new_main.py
+++ new_main.py
@@ -14,26 +14,52 @@ num_points = 100
 #Generate TaylorSeries object
 ts = taylor.TaylorSeries(N=N, x_min=x_min, x_max=x_max, num_points=num_points)
 
-fig = ts.create_fig(ts.cos_taylor())
-# fig = go.Figure()
-
-# for n in range(N):
-#     fig.add_trace(go.Scatter(x=ts.x_flat, y=df_cos[n], mode='lines', visible = True if n == 0 else False, name=f'N={n}'))
+#Create figures for selected functions
+fig_cos = ts.create_fig(ts.cos_taylor())
+fig_sin = ts.create_fig(ts.sin_taylor())
+fig_exp = ts.create_fig(ts.exp_taylor())
 
 app = Dash()
 
 app.layout = html.Div(children=[
     html.H1(children='Maclauren series intereactive examples'),
-    dcc.Graph(id='cos_graph', figure = fig),
-    dcc.Slider(0,N-1,1,value = 0, id='order')
+    dcc.Graph(id='cos_graph', figure = fig_cos),
+    dcc.Slider(0,N-1,1,value = 0, id='cos_slider'),
+
+    dcc.Graph(id='sin_graph', figure = fig_sin),
+    dcc.Slider(0,N-1,1,value = 0, id='sin_slider'),
+
+    dcc.Graph(id='exp_graph', figure = fig_exp),
+    dcc.Slider(0,N-1,1,value = 0, id='exp_slider')
+
+
     ])
+
 @callback(
     Output('cos_graph','figure'),
-    Input('order','value'))
+    Input('cos_slider','value'))
 def update_figure(selected_value):
 
-    for i, trace in enumerate(fig.data):
+    for i, trace in enumerate(fig_cos.data):
         trace.visible = True if i <= selected_value else False
-    return fig
+    return fig_cos
+
+@callback(
+    Output('sin_graph','figure'),
+    Input('sin_slider','value'))
+def update_figure(selected_value):
+
+    for i, trace in enumerate(fig_sin.data):
+        trace.visible = True if i <= selected_value else False
+    return fig_sin
+
+@callback(
+    Output('exp_graph','figure'),
+    Input('exp_slider','value'))
+def update_figure(selected_value):
+
+    for i, trace in enumerate(fig_exp.data):
+        trace.visible = True if i <= selected_value else False
+    return fig_exp
 
 app.run()
